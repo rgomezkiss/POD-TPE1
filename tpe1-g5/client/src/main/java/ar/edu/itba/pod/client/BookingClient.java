66ba789f9977d7b9d019f29da2510bdd92373f10
rgomezkiss <romgomez@itba.edu.ar>
2023-09-02 12:49:31 -0300

    Adding rough definitions of api methods and empty clients

--- tpe1-g5/client/src/main/java/ar/edu/itba/pod/client/BookingClient.java
+++ tpe1-g5/client/src/main/java/ar/edu/itba/pod/client/BookingClient.java
@@ -7,20 +7,36 @@ import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
-public class Client {
+public class AdminClient {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    private final static String[] actions = {"attractions", "availability", "book", "confirm", "cancel"};
 
     public static void main(String[] args) throws InterruptedException {
         logger.info("tpe1-g5 Client Starting ...");
         logger.info("grpc-com-patterns Client Starting ...");
+
+        String serverAddress = args[0];
+        String actionName = args[1];
+        String dayOfYear = args[2];
+        String rideName = args[3];
+        String visitorId = args[4];
+        String bookingSlotFrom = args[5];
+        String bookingSlotTo = args[6];
+
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
+
 
         try {
 
         } finally {
             channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
         }
+    }
+
+    private static boolean validateParameters()1 {
+//    Acá validamos primero que la acción tenga sentido y después vemos los parámetros
     }
 }
---
+++ tpe1-g5/client/src/main/java/ar/edu/itba/pod/client/ConsultClient.java
@@ -0,0 +1,41 @@
+package ar.edu.itba.pod.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class AdminClient {
+    private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    private final static String[] actions = {"capacity", "confirmed"};
+
+    public static void main(String[] args) throws InterruptedException {
+        logger.info("tpe1-g5 Client Starting ...");
+        logger.info("grpc-com-patterns Client Starting ...");
+
+        String serverAddress = args[0];
+        String actionName = args[1];
+        String dayOfYear = args[2];
+        String outputPath = args[3];
+
+
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+
+        try {
+
+        } finally {
+            channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
+        }
+    }
+
+    private static boolean validateParameters() {
+//    Acá validamos primero que la acción tenga sentido y después vemos los parámetros
+    }
+}
